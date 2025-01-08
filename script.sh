68e16d552846bc985bbda0849239604677f5d488
Faizan quazi <faizan_quazi@intuit.com>
2025-01-08 17:32:15 +0530

    space check function

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 
 
+
 function result(){
 curl -X GET "$1" \
 -H "Authorization: Bearer $SONAR_TOKEN"|jq -r '.task.status'
@@ -8,7 +9,7 @@ curl -X GET "$1" \
 function scan(){
 if mvn clean verify sonar:sonar -Dsonar.projectKey=springboot  \
  -Dsonar.projectName='springboot' -Dsonar.host.url=http://$SONAR_HOST \
- -Dsonar.token=$SONAR_TOKEN
+ -Dsonar.token=$SONAR_TOKEN >scan.log
 then
 echo "SCAN SUCCESS"
 return 0
@@ -18,6 +19,17 @@ fi
 }
 function clean(){
     mvn clean
+}
+function check_space(){
+     AVALIABLE_SPACE=$(df -h|grep -w "/"|awk '{print $2}'|sed 's/Gi//g')
+     if [ $AVALIABLE_SPACE -le $CHECK_SPACE ]
+     then
+     echo "no space to deploy"
+     return 1
+     else
+     echo "sufficient space to deploy"
+     return 0
+     fi
 }
 function deploy(){
 touch lock
