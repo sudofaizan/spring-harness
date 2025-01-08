31be67641e5205fbbeb04c711b73a4905f1bc388
Faizan quazi <faizan_quazi@intuit.com>
2025-01-08 19:00:08 +0530

    deplly optimize

--- script.sh
+++ script.sh
@@ -51,9 +51,11 @@ function deploy(){
     docker build -t springboot:$TAG .
     docker rm -f app
     export OLD_TAG=$(git rev-parse HEAD~2|cut -b 1-9)
+    git rev-parse HEAD~2|cut -b 1-9
     echo "old tag is $OLD_TAG"
     docker run -itd --name app -p 80:8080 springboot:$TAG
     export HOST=$(curl ifconfig.me)
+    curl ifconfig.me
 
     fi
     rm /home/ec2-user/lock
