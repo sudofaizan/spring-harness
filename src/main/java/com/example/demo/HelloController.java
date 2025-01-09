22cc61bd60f1f89b568b38a0570fd7aa272f6476
Faizan quazi <faizan_quazi@intuit.com>
2025-01-09 12:20:09 +0530

    changes in sc

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello, World! v2.0.9";
+        return "Hello, World! v3.0.0";
     }
 }
 
