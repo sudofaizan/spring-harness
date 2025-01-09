0f186c287805ed0ec90547535db5f92cf56e180b
Faizan quazi <faizan_quazi@intuit.com>
2025-01-09 11:06:52 +0530

    test changes

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello, World! v2.2.2";
+        return "Hello, World! v2.2.s2";
     }
 }
 
